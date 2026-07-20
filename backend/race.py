a6038153a9874e1e5de9f0824840701b4701af77
alexiszoungrana22-design <alexis.zoungrana22@gmail.com>
2026-07-20 20:17:52 +0000

    Create Race model for database

---
+++ backend/race.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String
+
+from database import Base
+
+
+class Race(Base):
+    __tablename__ = "races"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    date = Column(String)
+
+    hippodrome = Column(String)
+
+    race_name = Column(String)
+
+    distance = Column(String)
+
+    type_course = Column(String)
+
+    status = Column(
+        String,
+        default="Analyse en cours"
+    )
