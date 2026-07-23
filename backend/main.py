c3d7314c4c46c58a4480714f6c9272996856c1d5
alexiszoungrana22-design <alexis.zoungrana22@gmail.com>
2026-07-23 19:26:43 +0000

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,24 +2,22 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from api import router
-
+import os
 
 app = FastAPI(
     title="AZ Turf Pro API",
     version="1.0"
 )
 
-
 app.include_router(router)
 
 
-app.mount(
-    "/static",
-    StaticFiles(directory="frontend"),
-    name="static"
-)
+# API uniquement sur Render
+# Le frontend est hébergé par GitHub Pages
 
 
 @app.get("/")
 def accueil():
-    return FileResponse("frontend/index.html")
+    return {
+        "message": "AZ Turf Pro API en ligne"
+    }
