7ef758a71d0aa8f67a32819378c30219a70b31e7
alexiszoungrana22-design <alexis.zoungrana22@gmail.com>
2026-07-23 22:58:16 +0000

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -1,34 +1,32 @@
 from fastapi import APIRouter
 from engine import lancer_analyse
 
-
 router = APIRouter()
-
 
 
 @router.get("/analyse")
 def analyse():
 
-
     resultat = lancer_analyse()
-
 
     return {
 
         "message": "Analyse AZ Turf terminée",
 
+        "chevaux": resultat.get(
+            "classement",
+            []
+        ),
 
         "classement": resultat.get(
             "classement",
             []
         ),
 
-
         "favori": resultat.get(
             "favori",
             {}
         ),
-
 
         "tickets": resultat.get(
             "tickets",
