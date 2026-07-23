11f0e4890842b04b8700d80509c629e7d02b0a08
alexiszoungrana22-design <alexis.zoungrana22@gmail.com>
2026-07-23 08:54:39 +0000

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,20 +2,36 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from api import router
+import os
+
 
 app = FastAPI(
     title="AZ Turf Pro API",
     version="1.0"
 )
 
+
 # Routes API
 app.include_router(router)
 
-# Servir le frontend
-app.mount("/static", StaticFiles(directory="../frontend"), name="static")
+
+# Chemin absolu du frontend
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FRONTEND_DIR = os.path.join(BASE_DIR, "..", "frontend")
+
+
+# Fichiers CSS / JS
+app.mount(
+    "/static",
+    StaticFiles(directory=FRONTEND_DIR),
+    name="static"
+)
 
 
 # Page d'accueil
 @app.get("/")
 def accueil():
-    return FileResponse("../frontend/index.html")
+
+    return FileResponse(
+        os.path.join(FRONTEND_DIR, "index.html")
+    )
