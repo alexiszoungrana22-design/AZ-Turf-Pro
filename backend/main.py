30c5a455e624196f03a6bd00f6d7cc2532ddcb3d
alexiszoungrana22-design <alexis.zoungrana22@gmail.com>
2026-07-23 08:59:54 +0000

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from api import router
-import os
 
 
 app = FastAPI(
@@ -11,27 +10,16 @@ app = FastAPI(
 )
 
 
-# Routes API
 app.include_router(router)
 
 
-# Chemin absolu du frontend
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-FRONTEND_DIR = os.path.join(BASE_DIR, "..", "frontend")
-
-
-# Fichiers CSS / JS
 app.mount(
     "/static",
-    StaticFiles(directory=FRONTEND_DIR),
+    StaticFiles(directory="frontend"),
     name="static"
 )
 
 
-# Page d'accueil
 @app.get("/")
 def accueil():
-
-    return FileResponse(
-        os.path.join(FRONTEND_DIR, "index.html")
-    )
+    return FileResponse("frontend/index.html")
