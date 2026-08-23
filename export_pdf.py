77e083072a761f2f9ffc92ce66047906484b66c0
alexiszoungrana22-design <alexis.zoungrana22@gmail.com>
2026-08-23 15:45:47 +0000

    Add files via upload

---
+++ export_pdf.py
@@ -0,0 +1,2 @@
+def generer_pdf_ticket(data: dict) -> dict:
+    return {"status": "success", "url_pdf": ""}
---
+++ meteo_piste.py
@@ -0,0 +1,2 @@
+def analyser_impact_terrain(data: dict) -> dict:
+    return {"status": "success", "impact": "NEUTRE"}
